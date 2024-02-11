684c1bc1bd21681e03687797a9454a8fa34feaf0
Owen Chang <qzwx0310qzwx@gmail.com>
2024-02-12 01:58:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Owen Chang Monorepo
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./apps/**/*.{js,ts,jsx,tsx,mdx}",
+    "./packages/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
